4204ca275e782a5877654c697ecd2c88c7f3d8c2
EshaArya06-programmer <eshaarya06@gmail.com>
2026-09-15 14:05:15 +0000

    conversion of datatypes

---
+++ basics/conversionsOperations.js
@@ -0,0 +1,55 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);  // convert to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN => not a number
+
+// let score = null;
+// console.log(typeof score); // object
+
+
+// let valueInNumber = Number(score);  
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+//  let score = undefined;
+// console.log(typeof score); // undefined
+
+
+// let valueInNumber = Number(score);  
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// let score = true;
+// console.log(typeof score); // boolean
+
+
+// let valueInNumber = Number(score);  
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// let score = "Esha";
+// console.log(typeof score); // string
+
+
+// let valueInNumber = Number(score);  
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// let isLoggedIn = 1;
+
+// let boolenIsLoggedIn = Boolean(isLoggedIn); // convert to boolean
+// console.log(typeof boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; 
+// "Esha" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber); // convert to string
+console.log(typeof stringNumber);
+console.log(stringNumber);
---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3
+    +                   //code readability should be high
+    3);
+
+console.log("Esha");
+
+let name = "Esha";
+let age = 20;
+let isLggedIn = true;
+
+// primitive data types
+
+// number => 2 to power 53
+// bigInt
+// string => "" or '' 
+// boolean => true or false
+//null => standalone value
+// undefined => value not assigned
+// symbol => unique identifier
+
+// objects => key value pairs
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
